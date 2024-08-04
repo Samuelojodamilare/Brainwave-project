73599085a9f7952312853574859c89a0422103f8
Samuel Ojo Damilare <samueldamilare444@gmail.com>
2024-08-04 17:58:25 +0100

    add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefit from "./components/Benefit";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Section from "./components/Section";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefit />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
